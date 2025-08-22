a17944c80b4dc11a907a7bc5512aff0ed8ed6ede
Chibuokem Onyekwelu <chibuokem_tolu@hotmail.com>
2025-08-22 23:55:43 +0100

    Add keywords to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,25 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
 
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Chibuokem Onyekwelu",
   description: "Software Engineer (React, React Native, TypeScript, Next.js)",
+  keywords: [
+    "Chibuokem",
+    "Chibuokem Onyekwelu",
+    "Software Engineer",
+    "Senior Software Engineer",
+    "React",
+    "React Native",
+    "React Native Developer",
+    "React Native Engineer",
+    "TypeScript",
+    "JavaScript",
+    "Next.js",
+    "Mobile developer",
+  ],
+  creator: "Chibuokem Onyekwelu",
   icons: {
     icon: "/favicon.ico",
     apple: "/apple-touch-icon.png",
@@ -24,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
